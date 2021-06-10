fb5af93cc7fe3e8d02f8185b8a6b0b2b55fd55bd
Benedikt Ziemons <benedikt.ziemons@cern.ch>
2021-06-10 14:30:14 +0200

    Add other simple operations and run them in bulk

--- asciinema-editor.py
+++ asciinema-editor.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 
 import json
-import re
 import sys
+from typing import Tuple, List
 
 
 class Recording(object):
-
     def __init__(self, desc, lines):
         self.desc = desc
         self.lines = lines
 
     @classmethod
     def from_file(cls, filename):
-        with open(filename, "r", encoding='utf-8') as f:
+        with open(filename, "r", encoding="utf-8") as f:
             desc = json.loads(f.readline())
             lines = []
             for line in f.readlines():
@@ -21,61 +20,79 @@ class Recording(object):
             return cls(desc, lines)
 
     def write(self, filename):
-        with open(filename, 'w', encoding='utf-8') as f:
+        with open(filename, "w", encoding="utf-8") as f:
             desc_line = json.dumps(self.desc) + "\n"
             f.write(desc_line)
             for line in self.lines:
                 line = json.dumps(line) + "\n"
                 f.write(line)
 
-    def add_offset(self, offset):
-        for line in self.lines:
-            line[0] = line[0] + offset
+    def recalculate(self, operations: List[Tuple]):
+        # sort by end line
+        operations.sort(key=lambda x: x[1])
+        for op in operations:
+            if op[2] == "set":
+                start = op[0] - 2
+                end = op[1] - 2
+                difference = op[3] - self.lines[end][0]
+                for i in range(start, end + 1):
+                    self.lines[i][0] = op[3]
+                self.apply_offset(end + 1, difference)
+            elif op[2] == "offset":
+                start = op[0] - 2
+                self.apply_offset(start, op[3])
+            elif op[2] == "linear":
+                start = op[0] - 2
+                end = op[1] - 2
+                start_sec = self.lines[start][0]
+                old_ending_sec = self.lines[end][0]
+                for i in range(start + 1, end + 1):
+                    self.lines[i][0] = start_sec + (op[3] * (i - start))
+                new_ending_sec = self.lines[end][0]
+                self.apply_offset(end + 1, new_ending_sec - old_ending_sec)
+            elif op[2] == "timelapse":
+                start = op[0] - 2
+                end = op[1] - 2
+                start_sec = self.lines[start][0]
+                old_ending_sec = self.lines[end][0]
+                new_length_sec = op[3]
+                step_sec = new_length_sec / (old_ending_sec - start_sec)
+                for i in range(start + 1, end + 1):
+                    self.lines[i][0] = start_sec + (
+                        (self.lines[i][0] - start_sec) * step_sec
+                    )
+                new_ending_sec = self.lines[end][0]
+                self.apply_offset(end + 1, new_ending_sec - old_ending_sec)
 
-    def limit_idle_time(self, idle_time_limit, match):
-        last_time_old = 0
-        last_time_new = 0
-        for line in self.lines:
-            if match and not match.search(line[2]):
-                continue
-            curr_time_old = line[0]
-            diff = curr_time_old - last_time_old
-            diff = min(diff, idle_time_limit)
-            curr_time_new = last_time_new + diff
-            line[0] = curr_time_new
-            last_time_old = curr_time_old
-            last_time_new = curr_time_new
+    def apply_offset(self, start, offset):
+        for i in range(start, len(self.lines)):
+            self.lines[i][0] += offset
 
 
 def main():
-    op = sys.argv[1]
-    if op == "add-offset":
-        # Add an offset to all timestamps in a recording.
-        input_filename = sys.argv[2]
-        output_filename = sys.argv[3]
-        offset = float(sys.argv[4])
-        r = Recording.from_file(input_filename)
-        r.add_offset(offset)
-        r.write(output_filename)
-    elif op == "limit-idle-time":
-        # Limit idle time in a recording.
-        input_filename = sys.argv[2]
-        output_filename = sys.argv[3]
-        idle_time_limit = float(sys.argv[4])
-        if len(sys.argv) > 5:
-             match = re.compile(sys.argv[5])
-        else:
-             match = None
-        r = Recording.from_file(input_filename)
-        r.limit_idle_time(idle_time_limit, match)
-        r.write(output_filename)
-    elif op == "-h":
-        print("asciinema video editor")
-        print("Usage:")
-        print("  {} add-offset <input file> <output file> <offset>".format(sys.argv[0]))
-        print("  {} limit-idle-time <input file> <output file> <idle time limit> [<regex>]".format(sys.argv[0]))
-    else:
-        raise Exception("Unexpected operation: {}".format(op))
+    if len(sys.argv) < 3:
+        print(f"Usage: {sys.argv[0]} [input file] [output file]", file=sys.stderr)
+        sys.exit(1)
+
+    input_filename = sys.argv[1]
+    output_filename = sys.argv[2]
+    r = Recording.from_file(input_filename)
+    r.recalculate(
+        [
+            (7, 7, "set", 1.5),
+            (7, 22, "linear", 0.05),
+            (31, 52, "linear", 0.05),
+            (55, 55, "offset", -2),
+            (60, 60, "offset", -1),
+            (60, 79, "linear", 0.05),
+            (85, 85, "offset", -5),
+            (85, 120, "linear", 0.05),
+            (122, 145, "timelapse", 1),
+            (146, 173, "linear", 0.05),
+            (174, 657, "timelapse", 5),
+        ]
+    )
+    r.write(output_filename)
 
 
 if __name__ == "__main__":
